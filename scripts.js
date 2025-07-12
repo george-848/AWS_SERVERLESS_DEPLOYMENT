49bc2dcf58e12b9d69f3ac739ed40aaca96ae6aa
george-848 <appiahkubi848@gmail.com>
2025-07-12 15:44:32 +0000

    new commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "https://akpz2zu5s9.execute-api.us-east-1.amazonaws.com/prod";
+var API_ENDPOINT = "YOUR_API_GATEWAY";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
